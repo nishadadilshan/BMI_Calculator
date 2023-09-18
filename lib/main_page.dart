a126582e000872b2b615ff4cd3147fd1cc91c388
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2023-09-19 02:38:22 +0530

    UI changes

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -13,71 +13,81 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
      body: SafeArea(
-       child: Container(
-         color: Colors.white,
-         child: const Column(
-           children: [
-             Row(
-               children: [
-                 Padding(
-                   padding: EdgeInsets.all(10.0),
-                   child: Column(
-                     children: [
-                      Icon(Icons.male, size: 150,),
-                       Text("Male"),
-                     ],
+       child: Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 16),
+         child: Container(
+           color: Colors.white,
+           child: const Column(
+             children: [
+               Row(
+                 children: [
+                   Padding(
+                     padding: EdgeInsets.all(10.0),
+                     child: Column(
+                       children: [
+                        Icon(Icons.male, size: 150,),
+                         Text("Male"),
+                       ],
+                     ),
                    ),
-                 ),
-                 Spacer(),
-                 Padding(
-                   padding: EdgeInsets.all(10.0),
-                   child: Column(
-                     children: [
-                       Icon(Icons.female, size: 150,),
-                       Text("Female"),
-                     ],
+                   Spacer(),
+                   Padding(
+                     padding: EdgeInsets.all(10.0),
+                     child: Column(
+                       children: [
+                         Icon(Icons.female, size: 150,),
+                         Text("Female"),
+                       ],
+                     ),
+                   )
+                 ],
+               ),
+               SizedBox(height: 50,),
+               Row(
+                 children: [
+                   Padding(
+                     padding: EdgeInsets.all(10.0),
+                     child: Column(
+                       children: [
+                         Text("Height" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                         Text("176", style: TextStyle(color: Colors.blue, fontSize: 60, fontWeight:FontWeight.bold),),
+                         Row(
+                          children: [
+                            FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,),),
+                            SizedBox(width: 15),
+                            FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
+                          ],
+                         )
+                       ],
+                     ),
                    ),
-                 )
-               ],
-             ),
-             Row(
-               children: [
-                 Padding(
-                   padding: EdgeInsets.all(10.0),
-                   child: Column(
-                     children: [
-                       Text("Height" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                       Text("176", style: TextStyle(color: Colors.blue, fontSize: 60, fontWeight:FontWeight.bold),),
-                       Row(
-                        children: [
-                          FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,),),
-                          SizedBox(width: 15),
-                          FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
-                        ], 
-                       )
-                     ],
-                   ),
-                 ),
-                 Spacer(),
-                 Padding(
-                   padding: EdgeInsets.all(10.0),
-                   child: Column(
-                     children: [
-                       Text("Weight" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                       Text("90", style: TextStyle(color: Colors.blue, fontSize: 60, fontWeight:FontWeight.bold),),
-                       Row(
-                         children: [
-                         FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,)),
-                     SizedBox(width: 15),
-                     FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
-                     ],
-                       )
-                     ],
-                   ),
-                 )
-               ],
-             )
-           ],
+                   Spacer(),
+                   Padding(
+                     padding: EdgeInsets.all(10.0),
+                     child: Column(
+                       children: [
+                         Text("Weight" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                         Text("90", style: TextStyle(color: Colors.blue, fontSize: 60, fontWeight:FontWeight.bold),),
+                         Row(
+                           children: [
+                           FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,)),
+                       SizedBox(width: 15),
+                       FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
+                       ],
+                         )
+                       ],
+                     ),
+                   )
+                 ],
+               ),
+               Column(
+                 children: [
+                   Text("BMI"),
+                   Text("25.6")
+                 ],
+               )
+             ],
+           ),
          ),
        ),
      )
