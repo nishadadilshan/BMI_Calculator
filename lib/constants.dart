6c7e4e6b71775182df2ce917c92587112f98067b
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2023-09-19 21:52:47 +0530

    set the functionality to change the height and weight

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 const kTextColor = Colors.blue;
+const kOutPutColor = Colors.deepOrange;
+
 const kHeightWidthText = TextStyle(
     color: kTextColor,
     fontSize: 60,
+    fontWeight:FontWeight.bold
+);
+
+const kOutPutText = TextStyle(
+    color: kOutPutColor,
+    fontSize: 65,
     fontWeight:FontWeight.bold
 );
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,88 +10,175 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int height = 150;
+  int weight = 50;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: SafeArea(
-       child: Padding(
-         padding: const EdgeInsets.symmetric(horizontal: 16),
-         child: Container(
-           color: Colors.white,
-           child: const Column(
-             children: [
-               Row(
-                 children: [
-                   Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child: Column(
-                       children: [
-                        Icon(Icons.male, size: 150,),
-                         Text("Male"),
-                       ],
-                     ),
-                   ),
-                   Spacer(),
-                   Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child: Column(
-                       children: [
-                         Icon(Icons.female, size: 150,),
-                         Text("Female"),
-                       ],
-                     ),
-                   )
-                 ],
-               ),
-               SizedBox(height: 50,),
-               Row(
-                 children: [
-                   Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child: Column(
-                       children: [
-                         Text("Height" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                         Text("176", style: kHeightWidthText),
-                         Row(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        // padding for horizontal only.
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.male,
+                          size: 150,
+                        ),
+                        Text(
+                          "Male",
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.female,
+                          size: 150,
+                        ),
+                        Text(
+                          "Female",
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Height(cm)",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        Text("$height", style: kHeightWidthText),
+                        Row(
                           children: [
-                            FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,),),
-                            SizedBox(width: 15),
-                            FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  height--;
+                                });
+                                // print(height);
+                              },
+                              backgroundColor: Colors.redAccent,
+                              shape: const CircleBorder(),
+                              child: const Icon(
+                                Icons.remove,
+                                size: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 15),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  height++;
+                                });
+                              },
+                              backgroundColor: Colors.greenAccent,
+                              shape: const CircleBorder(),
+                              child: const Icon(
+                                Icons.add,
+                                size: 30,
+                              ),
+                            )
                           ],
-                         )
-                       ],
-                     ),
-                   ),
-                   Spacer(),
-                   Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child: Column(
-                       children: [
-                         Text("Weight" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                         Text("90", style: kHeightWidthText),
-                         Row(
-                           children: [
-                           FloatingActionButton(onPressed:null,backgroundColor: Colors.redAccent,shape:CircleBorder(), child: Icon(Icons.remove, size: 30,)),
-                       SizedBox(width: 15),
-                       FloatingActionButton(onPressed:null,backgroundColor: Colors.greenAccent,shape:CircleBorder(), child: Icon(Icons.add , size: 30,),)
-                       ],
-                         )
-                       ],
-                     ),
-                   )
-                 ],
-               ),
-               Column(
-                 children: [
-                   Text("BMI"),
-                   Text("25.6")
-                 ],
-               )
-             ],
-           ),
-         ),
-       ),
-     )
-    );
+                        )
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Weight(Kg)",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        Text("$weight", style: kHeightWidthText),
+                        Row(
+                          children: [
+                            FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                                backgroundColor: Colors.redAccent,
+                                shape: const CircleBorder(),
+                                child: const Icon(
+                                  Icons.remove,
+                                  size: 30,
+                                )),
+                            const SizedBox(width: 15),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              backgroundColor: Colors.greenAccent,
+                              shape: const CircleBorder(),
+                              child: const Icon(
+                                Icons.add,
+                                size: 30,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 50),
+              const Column(
+                children: [
+                  Text(
+                    "BMI",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    "25.6",
+                    style: kOutPutText,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
